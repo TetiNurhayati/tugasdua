8e8ee097958ea881d0e89af3214d98b8af0f3c7a
teti nurhayati <ntetdenok94@gmail.com>
2014-09-22 10:00:31 +0700

    commit pertama

---
+++ kali.java
@@ -0,0 +1,11 @@
+import java.util.*
+class kali 
+{
+	int a,b;
+	Scanner in=new Scanner(System);
+	System.out.println("A= ");
+	a=in nextInt;
+	System.out.println("B= ");
+	b=in nextInt;
+	System.out.println(a*b);
+}
